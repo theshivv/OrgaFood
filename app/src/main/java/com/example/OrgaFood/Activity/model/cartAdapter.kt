68bf72e8259a77b9a1fb6d0b6ae6e41568cd2e99
prathameshg121 <ghogarep2000@gmail.com>
2021-05-05 11:54:57 +0530

    cartEdit

---
+++ app/src/main/java/com/example/OrgaFood/Activity/model/cartAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.OrgaFood.Activity.model
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.OrgaFood.Activity.Info.cart
+import com.example.OrgaFood.R
+import kotlinx.android.synthetic.main.cart_layout.view.*
+
+class cartAdapter(
+    private val context : Context,
+    private val  cList : ArrayList<cart>
+): RecyclerView.Adapter<cartAdapter.cartViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): cartViewHolder {
+    var cartV  =  LayoutInflater.from(parent.context).inflate(R.layout.cart_layout,parent,false)
+
+return cartViewHolder(cartV)
+
+    }
+
+    override fun onBindViewHolder(holder: cartViewHolder, position: Int) {
+
+        val cartI =cList[position]
+        Glide.with(context).load(cartI.images).into(holder.itemView.cProductImg)
+
+
+        holder.itemView.cProductName.text= cartI.pName
+        holder.itemView.cPrice.text= cartI.price
+        holder.itemView.cSellerName.text= cartI.sellerName
+        holder.itemView.NumberOfItems.text=cartI.numOfItems
+        Log.i("adapter cart",cartI.pName)
+
+    }
+    override fun getItemCount(): Int {
+
+        return cList.size
+    }
+
+  inner class cartViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)   //passing the view in var itemView in Recycle view holder
+
+
+}
