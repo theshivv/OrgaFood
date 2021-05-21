371c661d1f27ade83e613ff93348aa3b0b27ca44
prathameshg121 <ghogarep2000@gmail.com>
2021-05-21 12:46:21 +0530

    BackEnd Updated

--- app/src/main/java/com/example/OrgaFood/Activity/Fragements/GetUserDataProfile.kt
+++ app/src/main/java/com/example/OrgaFood/Activity/Fragements/GetUserDataProfile.kt
@@ -3,6 +3,7 @@ package com.example.OrgaFood.Activity.Fragements
 import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,21 +11,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.OrgaFood.Activity.FireStore.FireStoreC
 import com.example.OrgaFood.Activity.Info.User
 import com.example.OrgaFood.R
 import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.fragment_get_user_data_profile.*
 import kotlinx.android.synthetic.main.fragment_get_user_data_profile.view.*
-import kotlinx.android.synthetic.main.fragment_profile_view.*
-import java.util.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -32,15 +29,21 @@ import java.util.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 //private val FS = FirebaseFirestore.getInstance()
+
 var firstN  = "null"
 var lastN = "null"
 var emailAdd = " "
 var phoneN = " "
 var address = " "
 var imageurl =" "
+var dateOfBirth = " "
+var gender =" "
+var idOfuse =" "
 lateinit var  filepath : Uri
-//lateinit var  cr : ContentResolver
 
+
+//lateinit var  cr : ContentResolver
+var imageUrlSt : String = "https://firebasestorage.googleapis.com/v0/b/orgafood-859fc.appspot.com/o/defaultProfilePic.png?alt=media&token=1948043e-2e8c-4137-85d9-dc4870528361"
 
 /**
  * A simple [Fragment] subclass.
@@ -60,11 +63,25 @@ class GetUserDataProfile : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+       /////Default Initialization when Image is not selected ///////
+        val builder = Uri.Builder()
+        builder.scheme("https")
+            .authority("www.myawesomesite.com")
+            .appendPath("turtles")
+            .appendPath("types")
+            .appendQueryParameter("type", "1")
+            .appendQueryParameter("sort", "relevance")
+            .fragment("section-name")
+        filepath = builder.build()
+/////////////////////////////////////////////////////////////////////////////
+
 
 
        val xmlData = inflater.inflate(R.layout.fragment_get_user_data_profile, container, false)
@@ -82,7 +99,7 @@ xmlData.saveChanges.setOnClickListener {
     Log.e(TAG, "onCreateView: save changes button is running")
     uploadImage(xmlData)
 
-
+onSNACK(xmlData)
 
 //
 }
@@ -115,32 +132,37 @@ xmlData.saveChanges.setOnClickListener {
 
 
 
-    fun getUserData(view: View, st : String){
+    fun getUserData(view: View, st: String){
 
 
             firstN = firstName.text.toString()
          emailAdd = emailAddressOfUser.text.toString()
          phoneN = phoneNo.text.toString()
         address = addressOfUser.text.toString()
+        dateOfBirth = birthdayid.text.toString()
+        imageurl = st
+        idOfuse = FireStoreC().getCurrentUID()
 
         val checkedId = view.radioGender.checkedRadioButtonId
         val radio: RadioButton = view.findViewById(checkedId)
-        var gender =  radio.text.toString()
+        gender =  radio.text.toString()
 
 
         Log.d(TAG, firstN + emailAdd + phoneN + address)
 
         val userInfo = User(
-            FireStoreC().getCurrentUID(),
+            idOfuse,
             firstN,
             lastN,
             emailAdd,
-            phoneN.toLong(),
+            phoneN,
             address,
             gender,
-          st
-
+            imageurl,
+            dateOfBirth
         )
+
+
 
         FireStoreC().completeTheUserProfile(userInfo)
     }
@@ -161,13 +183,25 @@ xmlData.saveChanges.setOnClickListener {
             val resolver = context?.applicationContext?.contentResolver
 
             var bitmap = MediaStore.Images.Media.getBitmap(resolver, filepath)
+            Log.e(TAG, "onActivityResult: $bitmap")
             ProfileImageView.setImageBitmap(bitmap)
+        }
+        else if (data == null){
+
+            ProfileImageView.setImageResource(R.drawable.ic_baseline_edit_24)
+            Log.d(TAG, "onActivityResult: image is not uploaded ")
+        }
+        else{
+
+
+            ProfileImageView.setImageResource(R.drawable.ic_baseline_edit_24)
+            Log.d(TAG, "onActivityResult: image is not uploaded ")
         }
     }
 
 
     private fun uploadImage(view: View)  {
-        var imageUrlSt : String = "https://firebasestorage.googleapis.com/v0/b/orgafood-859fc.appspot.com/o/defaultProfilePic.png?alt=media&token=1948043e-2e8c-4137-85d9-dc4870528361"
+
 
 
         if (filepath != null) {
@@ -179,27 +213,43 @@ xmlData.saveChanges.setOnClickListener {
                 .addOnSuccessListener { taskSnapshot ->
                     taskSnapshot.storage.downloadUrl.addOnSuccessListener {
                          imageUrlSt = it.toString()
-                        imageurl = it.toString()
-                        Log.d(TAG, "uploadImage: url issss " + imageurl,)
+
+                        Log.d(TAG, "uploadImage: url issss " + it.toString())
                         getUserData(view, it.toString())
 
                     }
                 }
 
                 ?.addOnFailureListener(OnFailureListener { e ->
-                    imageurl = imageUrlSt
                     print(e.message)
+
+                    getUserData(view, imageUrlSt)
                 })
-
-
         }
+        else{
 
-
-
-
+            getUserData(view, imageUrlSt)
+        }
     }
 
+    fun checkUserData() : Boolean{
 
+return  true
+    }
+
+    fun onSNACK(view: View){
+        //Snackbar(view)
+        val snackbar = Snackbar.make(view, "Successfully Updated",
+            Snackbar.LENGTH_LONG).setAction("Action", null)
+
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(Color.parseColor("#61bced"))
+        val textView =
+            snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
+        textView.setTextColor(Color.parseColor("#ffffff"))
+        textView.textSize = 10f
+        snackbar.show()
+    }
 
 }
 
--- app/src/main/java/com/example/OrgaFood/Activity/Info/User.kt
+++ app/src/main/java/com/example/OrgaFood/Activity/Info/User.kt
@@ -1,12 +1,16 @@
 package com.example.OrgaFood.Activity.Info
 
+import java.util.*
+
 class User (
     val id: String ="",
     val fName: String = "",
     val lName: String = "",
     val email : String = "",
-    val pNo : Long =0,
+    val pno : String = "",
     val address : String = "",
 val gender : String ="",
-val imageLink : String =""
+    val imageLink : String ="",
+    val dob : String=""
+
         )
